45afaadd7f1df4cee5df7b57f2fecd5c01cf7a24
J2517 <jackeline.rivera23296@ucaldas.edu.co>
2024-03-29 10:16:28 -0500

    MS-SECURITY

---
+++ ms-security/src/main/java/com/ucaldas/mssecurity/Controllers/SecurityController.java
@@ -0,0 +1,35 @@
+package com.ucaldas.mssecurity.Controllers;
+
+import com.ucaldas.mssecurity.Models.User;
+import com.ucaldas.mssecurity.Repositories.UserRepository;
+import com.ucaldas.mssecurity.services.JwtService;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import com.ucaldas.mssecurity.services.EncryptionService;
+
+import java.io.IOException;
+
+@CrossOrigin
+@RestController
+@RequestMapping("api/public/security")
+public class SecurityController {
+    @Autowired
+    private UserRepository theUserRepository;
+    @Autowired
+    private EncryptionService theEncryptionService;
+    @Autowired
+    private JwtService theJwtService;
+    @PostMapping("login")
+    public String login(@RequestBody User theUser, final HttpServletResponse response) throws IOException {
+        String token="";
+        User actualUser=this.theUserRepository.getUserByEmail(theUser.getEmail());
+        if(actualUser!=null &&
+                actualUser.getPassword().equals(this.theEncryptionService.convertSHA256(theUser.getPassword()))){
+            token=this.theJwtService.generateToken(actualUser);
+        }else{
+            response.sendError(HttpServletResponse.SC_UNAUTHORIZED);
+        }
+        return token;
+    }
+}
