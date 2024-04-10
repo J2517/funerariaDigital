2d074785b8402d51d294f6e429373261a52f5f01
Danielqh1060 <daniel.quintero31429@ucaldas.edu.co>
2024-04-09 21:48:22 -0500

    Security

--- ms-security/src/main/java/com/ucaldas/mssecurity/services/EmailService.java
+++ ms-security/src/main/java/com/ucaldas/mssecurity/services/EmailService.java
@@ -2,7 +2,6 @@ package com.ucaldas.mssecurity.services;
 
 import com.ucaldas.mssecurity.Models.Session;
 import com.ucaldas.mssecurity.Models.User;
-import ch.qos.logback.classic.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpEntity;
@@ -22,9 +21,6 @@ public class EmailService {
     @Value("${notifications.baseurl}")
     private String emailServiceUrl;
 
-    @Autowired
-    private Logger logger; // Suponiendo que tienes un componente para manejar los logs
-
     // Método para enviar correo electrónico para la autenticación de dos factores (2FA)
     public void send2FAEmail(User user, Session session) {
         Map<String, Object> emailContent = new HashMap<>();
@@ -32,29 +28,19 @@ public class EmailService {
         emailContent.put("subject", "Su token de 2FA");
         emailContent.put("plainText", "Su token es: " + session.getToken2FA());
 
-        try {
-            sendEmail(emailContent);
-            logger.info("Correo electrónico de 2FA enviado correctamente a {}", user.getEmail());
-        } catch (Exception e) {
-            logger.error("Error al enviar correo electrónico de 2FA a {}", user.getEmail(), e);
-        }
+        sendEmail(emailContent);
     }
 
     // Método para enviar correo electrónico para restablecimiento de contraseña
-    public void sendPasswordResetEmail(String email, String resetToken) {
+    public void sendPasswordResetEmail(String email, String Token) {
         Map<String, Object> emailContent = new HashMap<>();
         emailContent.put("address", email);
         emailContent.put("subject", "Restablecimiento de contraseña");
         emailContent.put("plainText", "Hemos recibido una solicitud para restablecer tu contraseña. Por favor, copia el siguiente TOKEN para restablecer su contraseña:\n\n"
-                + "TOKEN = " + resetToken + "\n\n"
+                + "TOKEN = " + Token + "\n\n"
                 + "Si no solicitaste un restablecimiento de contraseña, ignora este mensaje o contactate con soporte.\n\n");
 
-        try {
-            sendEmail(emailContent);
-            logger.info("Correo electrónico de restablecimiento de contraseña enviado correctamente a {}", email);
-        } catch (Exception e) {
-            logger.error("Error al enviar correo electrónico de restablecimiento de contraseña a {}", email, e);
-        }
+        sendEmail(emailContent);
     }
 
     // Método genérico para enviar correo electrónico
