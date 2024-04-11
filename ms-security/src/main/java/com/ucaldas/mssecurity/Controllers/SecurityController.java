2d1485668a3674925a2f943c726b49abf7923398
J2517 <jackeline.rivera23296@ucaldas.edu.co>
2024-04-11 00:18:03 -0500

    Implementación de recuperación de contraseña

--- ms-security/src/main/java/com/ucaldas/mssecurity/Controllers/SecurityController.java
+++ ms-security/src/main/java/com/ucaldas/mssecurity/Controllers/SecurityController.java
@@ -1,107 +1,3 @@
-// package com.ucaldas.mssecurity.Controllers;
-
-// import com.ucaldas.mssecurity.Models.Session;
-// import com.ucaldas.mssecurity.Models.User;
-// import com.ucaldas.mssecurity.Repositories.SessionRepository;
-// import com.ucaldas.mssecurity.Repositories.UserRepository;
-// import com.ucaldas.mssecurity.services.EncryptionService;
-// import com.ucaldas.mssecurity.services.JwtService;
-// import com.ucaldas.mssecurity.services.EmailService;
-// import jakarta.servlet.http.HttpServletResponse;
-// import org.springframework.beans.factory.annotation.Autowired;
-// import org.springframework.http.HttpStatus;
-// import org.springframework.http.ResponseEntity;
-// import org.springframework.web.bind.annotation.*;
-
-// import java.util.Map;
-
-// @CrossOrigin
-// @RestController
-// @RequestMapping("/api/public/security")
-// public class SecurityController {
-
-//     @Autowired
-//     private UserRepository userRepository;
-
-//     @Autowired
-//     private EncryptionService encryptionService;
-
-//     @Autowired
-//     private SessionRepository sessionRepository;
-
-//     @Autowired
-//     private JwtService jwtService;
-
-//     @Autowired
-//     private EmailService emailService;
-
-//     @PostMapping("login")
-//     public ResponseEntity<String> login(@RequestBody User user, HttpServletResponse response) {
-//         User actualUser = userRepository.getUserByEmail(user.getEmail());
-//         if (actualUser != null && actualUser.getPassword().equals(encryptionService.convertSHA256(user.getPassword()))) {
-//             String token = jwtService.generateToken(actualUser);
-//             verifyAndSend2FA(actualUser, token);
-//             return ResponseEntity.ok(token);
-//         } else {
-//             return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
-//         }
-//     }
-
-//     private void verifyAndSend2FA(User user, String token) {
-//         emailService.generateAndSend2FA(user, token); 
-//     }
-
-//     @PostMapping("login/2FA/{idUser}")
-//     public ResponseEntity<String> login2FA(@RequestBody Session session, @PathVariable String idUser) {
-//         Session actualSession = sessionRepository.getSessionByUser(idUser);
-//         if (actualSession != null && actualSession.getToken2FA() == session.getToken2FA()) {
-//             return ResponseEntity.ok("Conceder acceso.");
-//         } else {
-//             return ResponseEntity.ok("No permitir entrar.");
-//         }
-//     }
-
-//     @PostMapping("/forgot-password")
-//     public void forgotPassword(@RequestBody Map<String, String> requestBody) {
-//         String email = requestBody.get("email");
-//         User user = userRepository.getUserByEmail(email);
-//         if (user != null) {
-//             String resetToken = jwtService.generatePasswordToken(user);
-//             emailService.sendPasswordResetEmail(user.getEmail(), resetToken);
-//         }
-//     }
-
-//     @PostMapping("/reset-password")
-//     public void resetPassword(@RequestBody Map<String, String> requestBody) {
-//         String resetToken = requestBody.get("token");
-//         String newPassword = requestBody.get("newPassword");
-//         if (jwtService.validatePasswordToken(resetToken)) {
-//             String userEmail = jwtService.getUserIdEmailFromPasswordToken(resetToken);
-//             User user = userRepository.getUserByEmail(userEmail);
-//             if (user != null) {
-//                 user.setPassword(encryptionService.convertSHA256(newPassword));
-//                 userRepository.save(user);
-//             }
-//         }
-//     }
-
-//     @PostMapping("/change-password")
-//     public ResponseEntity<String> changePassword(@RequestBody Map<String, String> requestBody) {
-//         String email = requestBody.get("email");
-//         String currentPassword = requestBody.get("currentPassword");
-//         String newPassword = requestBody.get("newPassword");
-
-//         User user = userRepository.getUserByEmail(email);
-//         if (user != null && user.getPassword().equals(encryptionService.convertSHA256(currentPassword))) {
-//             user.setPassword(encryptionService.convertSHA256(newPassword));
-//             userRepository.save(user);
-//             return ResponseEntity.ok("Contraseña cambiada exitosamente");
-//         } else {
-//             return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Credenciales inválidas");
-//         }
-//     }
-// }
-//-----------------------------------------------------------------------------------------------------------------
 package com.ucaldas.mssecurity.Controllers;
 
 import com.ucaldas.mssecurity.Models.Session;
@@ -152,7 +48,7 @@ public class SecurityController {
             // Crear y guardar la sesión
             Session theUserSession = new Session();
             theUserSession.setUser(theActualUser); // Establece el usuario para la sesión
-            theUserSession.setToken2FA(token2FA); // Guarda el token 2FA
+            theUserSession.setToken2FA(theEncryptionService.convertSHA256(token2FA)); // Guarda el token 2FA
             theUserSession.setStartAt(new Date()); // Establece el tiempo actual como inicio
             theUserSession.setEndAt(new Date(System.currentTimeMillis() + (1000 * 60 * 60))); // Establece el tiempo de
                                                                                               // fin, podría ser una
--- ms-security/src/main/java/com/ucaldas/mssecurity/Controllers/UsersController.java
+++ ms-security/src/main/java/com/ucaldas/mssecurity/Controllers/UsersController.java
@@ -1,117 +1,10 @@
-// package com.ucaldas.mssecurity.Controllers;
-
-// import com.ucaldas.mssecurity.Models.Role;
-// import com.ucaldas.mssecurity.Models.User;
-// import com.ucaldas.mssecurity.Repositories.RoleRepository;
-// import com.ucaldas.mssecurity.Repositories.UserRepository;
-// import org.springframework.beans.factory.annotation.Autowired;
-// import org.springframework.http.HttpStatus;
-// import org.springframework.web.bind.annotation.*;
-// import com.ucaldas.mssecurity.services.EncryptionService;
-
-// import java.util.List;
-
-// @CrossOrigin
-// @RestController
-// @RequestMapping("api/users")
-// public class UsersCotroller {
-//     @Autowired
-//     private UserRepository theUserRepository;
-//     @Autowired
-//     private RoleRepository theRoleRepository;
-
-//     @Autowired
-//     private EncryptionService theEncryptionService;
-
-//     @GetMapping("")
-//     public List<User> findAll(){
-//         return this.theUserRepository.findAll();
-//     }
-//     @ResponseStatus(HttpStatus.CREATED)
-//     @PostMapping
-//     public User create(@RequestBody User theNewUser){
-//         theNewUser.setPassword(theEncryptionService.convertSHA256(theNewUser.getPassword()));
-//         return this.theUserRepository.save(theNewUser);
-//     }
-//     @GetMapping("{id}")
-//     public User findById(@PathVariable String id) {
-//         User theUser = this.theUserRepository
-//                 .findById(id)
-//                 .orElse(null);
-//         return theUser;
-//     }
-
-//     @PutMapping("{id}")
-//     public User update(@PathVariable String id, @RequestBody User theNewUser) {
-//         User theActualUser = this.theUserRepository
-//                 .findById(id)
-//                 .orElse(null);
-//         if (theActualUser != null) {
-//             theActualUser.setName(theNewUser.getName());
-//             theActualUser.setEmail(theNewUser.getEmail());
-//             theActualUser.setPassword(theEncryptionService.convertSHA256(theNewUser.getPassword()));
-//             return this.theUserRepository.save(theActualUser);
-//         } else {
-//             return null;
-//         }
-//     }
-
-
-//     @ResponseStatus(HttpStatus.NO_CONTENT)
-//     @DeleteMapping("{id}")
-//     public void delete(@PathVariable String id) {
-//         User theUser = this.theUserRepository
-//                 .findById(id)
-//                 .orElse(null);
-//         if (theUser != null) {
-//             this.theUserRepository.delete(theUser);
-//         }
-//     }
-
-//     @PutMapping("{userId}/role/{roleId}")
-//     public User matchRole(@PathVariable String userId,@PathVariable String roleId) {
-//         User theActualUser = this.theUserRepository
-//                 .findById(userId)
-//                 .orElse(null);
-//         Role theActualRole=this.theRoleRepository
-//                 .findById(roleId)
-//                 .orElse(null);
-
-//         if (theActualUser != null && theActualRole!=null) {
-//             theActualUser.setRole(theActualRole);
-//             return this.theUserRepository.save(theActualUser);
-//         } else {
-//             return null;
-//         }
-//     }
-
-//     @PutMapping("{userId}/unmatch-role/{roleId}")
-//     public User unMatchRole(@PathVariable String userId,@PathVariable String roleId) {
-//         User theActualUser = this.theUserRepository
-//                 .findById(userId)
-//                 .orElse(null);
-//         Role theActualRole=this.theRoleRepository
-//                 .findById(roleId)
-//                 .orElse(null);
-
-//         if (theActualUser != null
-//                 && theActualRole!=null
-//                 && theActualUser.getRole().get_id().equals(roleId)) {
-//             theActualUser.setRole(null);
-//             return this.theUserRepository.save(theActualUser);
-//         } else {
-//             return null;
-//         }
-//     }
-
-// }
-
 package com.ucaldas.mssecurity.Controllers;
 
 import com.ucaldas.mssecurity.Models.Role;
 import com.ucaldas.mssecurity.Models.User;
 import com.ucaldas.mssecurity.Repositories.RoleRepository;
 import com.ucaldas.mssecurity.Repositories.UserRepository;
+import com.ucaldas.mssecurity.services.EmailService;
 import com.ucaldas.mssecurity.services.EncryptionService;
 import com.ucaldas.mssecurity.services.PasswordService;
 
@@ -137,6 +30,9 @@ public class UsersController {
     @Autowired
     private PasswordService thePasswordService;
 
+    @Autowired
+    private EmailService theEmailService;
+
     @GetMapping("")
     public List<User> findAll() {
         return this.theUserRepository.findAll();
@@ -145,12 +41,17 @@ public class UsersController {
     @ResponseStatus(HttpStatus.CREATED)
     @PostMapping
     public User create(@RequestBody User theNewUser) {
-        // asigna como contraseña el token generado en el servicio de passwordService
-        String tokenPassword = thePasswordService.generateToken();
-        System.out.println("Token generado: " + tokenPassword);
-        theNewUser.setPassword(theEncryptionService.convertSHA256(tokenPassword));
+        theNewUser.setPassword(theEncryptionService.convertSHA256(theNewUser.getPassword()));
         return this.theUserRepository.save(theNewUser);
     }
+    // public User create(@RequestBody User theNewUser) {
+
+    // // asigna como contraseña el token generado en el servicio de passwordService
+    // String tokenPassword = thePasswordService.generateToken();
+    // System.out.println("Token generado: " + tokenPassword);
+    // theNewUser.setPassword(theEncryptionService.convertSHA256(tokenPassword));
+    // return this.theUserRepository.save(theNewUser);
+    // }
 
     @GetMapping("{id}")
     public User findById(@PathVariable String id) {
@@ -222,5 +123,23 @@ public class UsersController {
             return null;
         }
     }
+    /*Recuperar contraseña: por medio de una petición POST donde solo se recibe como cuerpo del JSON 
+    el correo del usuario se debe enviar un correo con la contraseña del usuario, dicha contraseña se 
+    genera en el servicio de PasswordService y se guarda en la base de datos encriptada con SHA256.
+    se deben actualizar los datos del usuario en la base de datos con la nueva contraseña generada.*/
 
+    @PostMapping("/recover-password")
+    public void recoverPassword(@RequestBody User theUser) {
+        User theActualUser = this.theUserRepository.getUserByEmail(theUser.getEmail());
+        if (theActualUser != null) {
+            // Generar contraseña
+            String tokenPassword = thePasswordService.generateToken();
+            System.out.println("Token generado: " + tokenPassword);
+            theActualUser.setPassword(theEncryptionService.convertSHA256(tokenPassword));
+            this.theUserRepository.save(theActualUser);
+            // Enviar contraseña por correo
+            this.theEmailService.sendEmail(theActualUser.getEmail(), "Recuperación de contraseña",
+            "Su nueva contraseña es: " + tokenPassword);
+        }
+    }
 }
