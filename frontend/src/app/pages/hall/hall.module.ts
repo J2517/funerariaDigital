8c8c55ed8dd1c0fce46370566982652b8c3f0a58
Danielqh1060 <daniel.quintero31429@ucaldas.edu.co>
2024-05-01 18:32:53 -0500

    modulos

---
+++ frontend/src/app/pages/hall/hall.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { HallRoutingModule } from './hall-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    HallRoutingModule
+  ]
+})
+export class HallModule { }
