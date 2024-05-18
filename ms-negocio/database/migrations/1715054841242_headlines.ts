4e30b865c7b157897c5d62d07f8fa591d692d9b7
Danielqh1060 <daniel.quintero31429@ucaldas.edu.co>
2024-05-18 08:39:58 -0500

    ...

--- ms-negocio/database/migrations/1715054841242_headlines.ts
+++ ms-negocio/database/migrations/1715054841242_headlines.ts
@@ -6,11 +6,7 @@ export default class extends BaseSchema {
   public async up() {
     this.schema.createTable(this.tableName, (table) => {
       table.increments("id");
-<<<<<<< HEAD
       table.string("tipo_plan").notNullable();
-=======
-       table.string("tipo_plan").notNullable();
->>>>>>> de2377a85288e2675aac302b1161fd043a75663a
       table.integer("user_id").unsigned().references("users.id").onDelete("CASCADE");
       table.timestamp("created_at", { useTz: true });
       table.timestamp("updated_at", { useTz: true });
